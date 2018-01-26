935953f64b6fc8d06836e3743494f3c03db68c6b
Ivo Leko <ivoleko@gmail.com>
2018-01-26 15:00:04 +0100

    Xcode project startup

---
+++ Peercoin Wallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Peercoin Wallet
+//
+//  Created by Ivo Leko on 26/01/2018.
+//  Copyright © 2018 Ivo Leko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Peercoin Wallet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Peercoin Wallet
+//
+//  Created by Ivo Leko on 26/01/2018.
+//  Copyright © 2018 Ivo Leko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
