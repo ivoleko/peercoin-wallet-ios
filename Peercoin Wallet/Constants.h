9d967c9e3013d6f5e1378aa3b73736ebcc371380
Ivo Leko <ivoleko@gmail.com>
2018-01-26 15:44:29 +0100

    Added LaunchScreen and AppIcon.

---
+++ Peercoin Wallet/Constants.h
@@ -0,0 +1,25 @@
+//
+//  Constants.h
+//  Peercoin Wallet
+//
+//  Created by Ivo Leko on 26/01/2018.
+//  Copyright © 2018 Ivo Leko. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+//
+// COLORS
+//
+#define kPPCColor_dark       [UIColor colorWithRed:51.0/255.0 green:51.0/255.0 blue:51.0/255.0 alpha:1.0];
+#define kPPCColor_green      [UIColor colorWithRed:60.0/255.0 green:176.0/255.0 blue:84.0/255.0 alpha:1.0];
+#define kPPCColor_white      [UIColor whiteColor]
+#define kPPCColor_grey       [UIColor colorWithRed:99.0/255.0 green:99.0/255.0 blue:99.0/255.0 alpha:1.0];
+#define kPPCColor_lightGrey  [UIColor colorWithRed:239.0/255.0 green:239.0/255.0 blue:239.0/255.0 alpha:1.0];
+
+
+
+
+
+#endif /* Constants_h */
